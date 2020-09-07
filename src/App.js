9918a463bdcb7dcfe7f5ab08792c0fd3533ae328
cameron cheng <cameroncheng@gmail.com>
2020-09-07 14:53:11 -0700

    create removeNomination functionality

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
-import React, { useState } from 'react';
-import Search from './Search'
-import Nomination from './Nomination'
-import './App.css';
+import React, { useState } from "react";
+import Search from "./Search";
+import Nomination from "./Nomination";
+import "./App.css";
 
 function App() {
-  const [nominationList, setNominationList] = useState([])
+  const [nominationList, setNominationList] = useState([]);
   const checkNominations = (movieID) => {
-    const movieIDs = nominationList.map(movie =>  movie.imdbID
-    )
-    if (movieIDs.includes(movieID))
-      return false;
+    const movieIDs = nominationList.map((movie) => movie.imdbID);
+    if (movieIDs.includes(movieID)) return false;
     if (nominationList.length < 5) {
       return true;
     }
-    return false
-  }
+    return false;
+  };
   const nominate = (movie) => {
-    setNominationList([...nominationList, movie])
-  }
+    setNominationList([...nominationList, movie]);
+  };
+
+  const removeNomination = (movieToRemove) => {
+    const removeNominationList = [...nominationList]; 
+
+    const nominationIndex = removeNominationList.findIndex(
+      nomination => nomination.imdbID === movieToRemove
+    );
+    removeNominationList.splice(nominationIndex, 1);
+    setNominationList(removeNominationList);
+
+  };
 
   return (
     <div>
-      
-      <Search  checkNominations={checkNominations} nominate={nominate}/>
-      <Nomination nominations={nominationList}/>
-
+      <Search checkNominations={checkNominations} nominate={nominate} />
+      <Nomination
+        nominations={nominationList}
+        removeNomination={removeNomination}
+      />
     </div>
-      
-
-
   );
 }
 
--- src/Nomination.js
+++ src/Nomination.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Nomination({ nominations }) {
+function Nomination({ nominations, removeNomination }) {
   console.log('nominations :>> ', nominations);
   const nominees = nominations.map((movie) => {
     return (
-      <li> 
-        <p>{movie.Title}</p>
-        <button> Remove </button>
+      <li key={movie.imdbID}> 
+        <p>{movie.Title} {movie.Year} </p>
+        <button onClick={() => {removeNomination(movie.imdbID)}}> Remove </button>
       </li>
     )
   })
