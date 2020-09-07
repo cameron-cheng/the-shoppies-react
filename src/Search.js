587b2bd1d14cf983eae5d7a960eb2ab8934cbc24
cameron cheng <cameroncheng@gmail.com>
2020-09-06 22:55:48 -0700

    get api results and list display them

--- src/Search.js
+++ src/Search.js
@@ -1,31 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const axios = require('axios');
-const API_KEY = '45aaa6eb'
+const axios = require("axios");
+const API_KEY = "45aaa6eb";
 
-const apiURL = `https://www.omdbapi.com/?apikey=${API_KEY}` 
+const apiURL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
 function Search() {
+  const [movieTitle, setMovieTitle] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-  const [movieTitle, setMovieTitle] = useState('');
+  const search = async () => {
+    try {
+      const response = await axios.get(`${apiURL}&s=${movieTitle}`);
+      console.log(response.data.Search);
+      setSearchResults(response.data.Search);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-  const search = async (movieTitle) => {
-      try {
-        const response = await axios.get(`${apiURL}&s=${movieTitle}`);
-        console.log(response);
-      } catch (error) {
-        console.error(error);
-      }
-  }
-  
-  
+  const movies = searchResults.map((movie) => {
+    return (
+      <li key={movie.imdbID}>
+        <p>{movie.Title} ({movie.Year})</p>
+        <button> Nominate </button>
+      </li> 
+    )
+  })
+
   return (
     <div>
-      
-        <input type="text" name="movieTitle" onChange={ value => setMovieTitle(value) }value={ movieTitle } />
+      <input
+        type="text"
+        name="movieTitle"
+        onChange={(event) => {
+          setMovieTitle(event.target.value);
+        }}
+        value={movieTitle}
+      />
 
-        <button onClick={ search }>Submit</button>
-   
+      <button onClick={search}>Submit</button>
+
+      <h2>Search Results</h2>
+      <ul> {movies} </ul>
     </div>
   );
 }
