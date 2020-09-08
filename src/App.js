b1dc34e3005aa9670b429b585920a3380369d8e7
cameron cheng <cameroncheng@gmail.com>
2020-09-07 19:29:07 -0700

    implement local storage for data to persist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import Search from "./Search";
 import Nomination from "./Nomination";
-import Banner from "./Banner"
+import Banner from "./Banner";
 import "./App.css";
 
-
 function App() {
-  const [nominationList, setNominationList] = useState([]);
-  
+  const [nominationList, setNominationList] = useState(() => {
+    const storedNominationList = localStorage.getItem("nominationList");
+    return storedNominationList !== null
+      ? JSON.parse(storedNominationList)
+      : [];
+  });
+
   const checkNominations = (movieID) => {
     const movieIDs = nominationList.map((movie) => movie.imdbID);
     if (movieIDs.includes(movieID)) return false;
@@ -16,24 +20,30 @@ function App() {
     }
     return false;
   };
+
   const nominate = (movie) => {
     setNominationList([...nominationList, movie]);
+    console.log("nominationList :>> ", nominationList);
+    localStorage.setItem(
+      "nominationList",
+      JSON.stringify([...nominationList, movie])
+    );
   };
 
   const removeNomination = (movieToRemove) => {
-    const removeNominationList = [...nominationList]; 
+    const removeNominationList = [...nominationList];
 
     const nominationIndex = removeNominationList.findIndex(
-      nomination => nomination.imdbID === movieToRemove
+      (nomination) => nomination.imdbID === movieToRemove
     );
     removeNominationList.splice(nominationIndex, 1);
     setNominationList(removeNominationList);
-
+    localStorage.setItem("nominationList", JSON.stringify(removeNominationList));
   };
 
   return (
     <div>
-      <Banner nominationList={nominationList}/>
+      <Banner nominationList={nominationList} />
       <Search checkNominations={checkNominations} nominate={nominate} />
       <Nomination
         nominations={nominationList}
